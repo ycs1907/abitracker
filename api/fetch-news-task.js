e7f02477d07d6a2caa248e39cacb9d67a48da262
ycs1907 <63986692+ycs1907@users.noreply.github.com>
2026-02-02 03:53:31 +0300

    Delete api/fetch-news-task.js

--- api/fetch-news-task.js
+++
@@ -1,6 +0,0 @@
-// Arka planda çalışacak olan kod
-export default async function handler(req, res) {
-  const news = await getNews(); // Senin mevcut getNews fonksiyonun
-  await kv.set('news', news);   // Veriyi kalıcı depoya yaz
-  res.status(200).send('Haberler güncellendi!');
-}
